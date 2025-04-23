467faee14eab55271e7bd25a4c7a62ef45775014
hnsqls <2661108038@qq.com>
2025-04-23 13:59:15 +0800

    add springai 提供的日志

--- shuo-ai-backend/src/main/java/com/ls/ai/config/AIConfiguration.java
+++ shuo-ai-backend/src/main/java/com/ls/ai/config/AIConfiguration.java
@@ -1,6 +1,7 @@
 package com.ls.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ public class AIConfiguration {
     public ChatClient chatClient(OllamaChatModel model) {
         return ChatClient
                 .builder(model)
+                .defaultAdvisors(new SimpleLoggerAdvisor())  // 日志
                 .build();
     }
 }
